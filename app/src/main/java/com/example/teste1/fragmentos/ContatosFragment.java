ec1a7973f63e6b2f567e3add7c611949b07dbd47
Magno Costa <magnocp123@gmail.com>
2020-02-12 15:13:06 -0300

    Alterado local de arquivos!

--- app/src/main/java/com/example/teste1/fragmentos/ContatosFragment.java
+++ app/src/main/java/com/example/teste1/fragmentos/ContatosFragment.java
@@ -1,4 +1,4 @@
-package com.example.teste1;
+package com.example.teste1.fragmentos;
 
 
 import android.os.Bundle;
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.teste1.R;
 
 
 /**
